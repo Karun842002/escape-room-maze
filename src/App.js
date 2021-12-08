47394b7dee2a0f785d09d1c1d9a4aa66c81e251f
karun <karun19049@cse.ssn.edu.in>
2021-12-08 15:40:38 +0530

    Working Prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { maze } from './walls'
+import UpButton from './Up'
+import DownButton from './Down'
+import LeftButton from './Left'
+import RightButton from './Right'
 
 function App() {
+  const [hero,setHero] = useState([1,0])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="maze_container">
+      <div id="maze">
+        {maze.map(function(row,i){
+          return(
+          <div>
+            {row.map(function(col,j) {
+              if(i===hero[0] && j===hero[1]) 
+              return (<div class="hero"></div>)
+              if(maze[i][j].wall === true)
+              return (<div class="wall"></div>)
+              else
+              return(<div></div>)
+            })}
+          </div>)
+        })}
+      </div>
+      <div class="buttoncon">
+      <UpButton hero={hero} setHero={setHero}></UpButton>
+      <DownButton hero={hero} setHero={setHero}></DownButton>
+      <LeftButton hero={hero} setHero={setHero}></LeftButton>
+      <RightButton hero={hero} setHero={setHero}></RightButton>
+      </div>
     </div>
   );
 }
---
+++ src/Left.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { maze } from './walls'
+class LeftButton extends React.Component {
+  state = {
+    open: false,
+    pos : this.props.hero[1]
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+    var hero = this.props.hero
+    for(var j=hero[1];j>=0;j--){
+      console.log(maze)
+      if(maze[hero[0]][j-1].wall===true){
+        break;
+      }
+    }
+    console.log(j)
+    this.setState({ pos: j });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleAgree = () => {
+    console.log("I agree!");
+    this.props.setHero([this.props.hero[0],this.state.pos]);
+    this.handleClose();
+  };
+  handleDisagree = () => {
+    console.log("I do not agree.");
+    this.handleClose();
+  };
+  render() {
+    return (
+      <div>
+        {/* Button to trigger the opening of the dialog */}
+        <Button onClick={this.handleClickOpen}>Left</Button>
+        {/* Dialog that is displayed if the state open is true */}
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Successful Alert"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+            Are you sure you want to move LEFT?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleDisagree} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={this.handleAgree} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default LeftButton;
