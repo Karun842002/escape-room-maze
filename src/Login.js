ee1e66d699c2dfecfaa376b8d253d2b0e69a3183
karun <karun19049@cse.ssn.edu.in>
2021-12-09 12:18:11 +0530

    Animations

--- src/Login.js
+++ src/Login.js
@@ -2,28 +2,25 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from './Button';
+import Particle from "./Particle";
+import Particle2 from './Particle2'
+import './login.css'
+
 export default function BasicTextFields({ setPassword, setEmail, handleAction }) {
     return (
         <div>
-            <div className="heading-container">
-                <h3>
+            <Particle2 />
+            <div class="login-form">
+                <Particle/>
+                <div className="heading-container" id="heading">
                     Login Form
-                </h3>
+                </div>
+                <div id="textfield"><TextField id="email" label="Enter the Email" variant="outlined" onChange={(e) => setEmail(e.target.value)}/></div>
+                <div id="textfield"><TextField id="password" label="Enter the Password" variant="outlined" onChange={(e) => setPassword(e.target.value)}/></div>
+                <Button handleAction={handleAction}/>
             </div>
-
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField id="email" label="Enter the Email" variant="outlined" onChange={(e) => setEmail(e.target.value)}/>
-                <TextField id="password" label="Enter the Password" variant="outlined" onChange={(e) => setPassword(e.target.value)}/>
-            </Box>
-
-            <Button handleAction={handleAction}/>
+                
+            
         </div>
     );
 }
